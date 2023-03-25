f171a59c6fbb1c2334069d1d1763935729de3328
Mr-logic1148 <mehrajgaud1148@gmail.com>
2023-03-25 18:36:15 +0000

    node changes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get("/ping", (req, res) => {
+    res.send("pong");
+});
+
 app.listen(port, () => {
-    console.log('Server running');
+    console.log('Server running on port ${port}');
 });
