8ebdf00f5e12461e3c07430342af9b6986b5fbba
Mr-logic1148 <mehrajgaud1148@gmail.com>
2023-03-28 19:01:00 +0100

    backend

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-const express = require('express');
-
+const express = require("express");
+const mysql = require("mysql2");
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('WELCOME TO DATA POPULAS!');
 });
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST || "localhost",
+    user: "user",
+    password: "password",
+    database: "world",
+});
+
 
 app.get("/ping", (req, res) => {
     res.send("pong");
 });
 
+app.get("/cities", (req, res) => {
+    db.execute("SELECT * FROM `city`", (err, rows, fields) => {
+        console.log(`/cities: ${rows.length} rows`);
+        return res.send(rows);
+    });
+});
+
 app.listen(port, () => {
-    console.log('Server running on port ${port}');
+    console.log(`Server running on port ${port}`);
 });
