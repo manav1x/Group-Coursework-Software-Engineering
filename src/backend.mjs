b72b02336dbaf0e197b2edcd8e196e4d21985dc1
manavx1 <mdoshi452@gmail.com>
2023-04-24 23:24:37 +0100

    Revert "backend and frontend comment"
    This reverts commit 5818c8af6424e91a1bbbd316c7a8a658ed601306.

--- src/backend.mjs
+++ src/backend.mjs
@@ -1,38 +1,20 @@
-//Import all the modules
 import express from "express";
 import mysql from "mysql2/promise";
 
-//Create an instance for express and set port to 4000
+
 const app = express();
 const port = 4000;
 
-//app.use(express.urlencoded({extended: true}));
-
-//Set the view engine to pug
 app.set("view engine", "pug");
 
-//Use the static files form static dictionary
 app.use(express.static("static"));
 
-//Print the value of the NODE_ENV
 console.log(process.env.NODE_ENV);
 
-
-//Handle request to the root URL
-app.get("/", (req,res) =>{
+app.get('/', (req, res) => {
     res.render("index");
 });
 
-/*app.get('/register', function(res,req){
-    res.render('register');
-
-});
-
-app.get('/login', function(res,req){
-    res.render('login');
-});*/
-
-//create a MYSQL database coonnection
 const db = await mysql.createConnection({ 
     host: process.env.DATABASE_HOST || "localhost",
     user: "user",
@@ -40,42 +22,10 @@ const db = await mysql.createConnection({
     database: "world",
 });
 
-app.get("/cities", async (req, res) => {
-    try{
-        const [rows, fields] = await db.execute("SELECT * FROM `city`");
-        console.log(`/cities: ${rows.length} rows`);
-        return res.render("cities", {rows, fields});
-    } catch (err) {
-        console.error(err);
-        return res.send("Internal Server Error");
-    }
-
+app.get("/ping", (req, res) => {
+    res.send("pong");
 });
 
-app.get("/country", async (req, res) => {
-    try {
-        const [rows, fields] = await db.execute("SELECT * FROM `country`");
-        //console.log(`/country: ${rows.length} rows`);
-        return res.render("country", {rows, fields});
-    } catch (err) {
-        console.error(err);
-        return res.send("Internal Server Error");
-    }
-});
-
-app.get("/language", async (req, res) => {
-    try {
-        const [rows, fields] = await db.execute("SELECT * FROM `countrylanguage`");
-        //console.log(`/language: ${rows.length} rows`);
-        return res.render("language", {rows, fields});
-    } catch (err) {
-        console.error(err);
-        return res.send("Internal Server Error");
-    }
-});
-
-/*
-//Handle request to the cities URL
 app.get("/cities", async (req, res) => { 
     try{
         const [rows, fields] = await db.execute("SELECT * FROM `city`");
@@ -88,7 +38,6 @@ app.get("/cities", async (req, res) => {
 
 });
 
-//Handle request to the country URL
 app.get("/country", async (req, res) => { 
     try {
         const [rows, fields] = await db.execute("SELECT * FROM `country`"); 
@@ -100,8 +49,6 @@ app.get("/country", async (req, res) => {
             }
 });
 
-
-//Handle request to the language URL
 app.get("/language", async (req, res) => { 
     try {
         const [rows, fields] = await db.execute("SELECT * FROM `countrylanguage`"); 
@@ -112,7 +59,7 @@ app.get("/language", async (req, res) => {
         return res.send("Internal Server Error"); 
     }
 });
-*/
+
 
 /*app.post('/cities/:id', async (req, res) => {
     const cityId  = req.params.id;
@@ -128,7 +75,6 @@ app.get("/language", async (req, res) => {
 
 
 
-//Start the Express application and listen for incoming requests
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
